c4df96feee508877ef98a93a111c483ef667a91c
Coding_Buddha <priyanshraj3020@gmail.com>
2023-06-09 16:12:57 +0530

    Day~6

---
+++ javascript/Objects/app.js
@@ -0,0 +1,21 @@
+// Let's talk about object
+
+const house = {
+    Harry : "Gryfindor",
+    Luna  : "Ravenclaw",
+    Cedric: "Hufflepuff"
+};
+
+// Object can have mixed datatype element in it
+const person = {
+    name : "Priyansh",
+    Git_username : "Coding_Buddha",
+    Isfunny : true,
+    Bd_date : [30,"May",2004]
+};
+
+// Accessing properties 
+
+    console.log(person["name"]); //we only use {} when defining it, we use [] when we want to access it  
+
+    console.log(person.Bd_date);
